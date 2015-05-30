e71d5f06762e7b6fb47423262100d77d5c607a55
areinull <a.rei.null@gmail.com>
2015-05-30 21:19:20 +0300

    correct and fast with preprocessing

--- week6/randalg.py
+++ week6/randalg.py
@@ -31,10 +31,45 @@ class Clause:
 	def __init__(self, x, y):
 		self.x = abs(x)
 		self.y = abs(y)
+		self.x_ = x
+		self.y_ = y
 		self.fn = self.getFn(x,y)
+
+	def __str__(self):
+		return 'Clause: ' + str(self.x_) + ' '  + str(self.y_)
 
 	def check(self, A):
 		return self.fn(A[self.x],A[self.y])
+
+def preprocess(C):
+	while True:
+		print 'C size:', len(C)
+		cc = {}
+		used = set()
+		for i,c in enumerate(C):
+			if c.x not in used:
+				if -c.x_ in cc:
+					del cc[-c.x_]
+					used.add(c.x)
+				else:
+					if c.x_ not in cc:
+						cc[c.x_] = []
+					cc[c.x_].append(i)
+			if c.y not in used:
+				if -c.y_ in cc:
+					del cc[-c.y_]
+					used.add(c.y)
+				else:
+					if c.y_ not in cc:
+						cc[c.y_] = []
+					cc[c.y_].append(i)
+		if len(cc) == 0:
+			return C
+		for k in cc:
+			for i in cc[k]:
+				C[i] = None
+		C = [e for e in C if e is not None]
+						
 
 C = []
 with open(sys.argv[1],'r') as infile:
@@ -47,12 +82,18 @@ with open(sys.argv[1],'r') as infile:
 
 assert len(C) == N
 
-res = False
+C = preprocess(C)
+
+if len(C) == 0:
+	res = True
+else:
+	res = False
 totalLoop = int(math.ceil(math.log(N,2)))
-totalInner = 2*N*N
+totalInner = 2*N
+
 for i in xrange(totalLoop):
-	print 'Outer loop %d of %d' % (i+1, totalLoop)
 	if res: break
+	print 'Outer loop %d of %d' % (i+1, totalLoop)
 	A = [random.choice((True,False)) for _ in xrange(N+1)]
 	prevProg = -1
 	for j in xrange(totalInner):
